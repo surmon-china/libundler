1f2e7e924e81695b0c79ca3beaf32e5d7e6a544d
surmon-china <surmon@foxmail.com>
2020-03-16 01:24:15 +0800

    Remove shelljs

--- bin/abc.js
+++ bin/abc.js
@@ -1,22 +1,29 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const shelljs = require('shelljs')
+const consola = require('consola')
+const child_process = require('child_process')
 const { COMMAND } = require('../lib/constants')
 const [commandName, ...commandParams] = process.argv.slice(2)
 
+const run = (command, params) => {
+  if (command) {
+    consola.info(`${command} ${params.join(' ')}`)
+    child_process.execFileSync(command, params, { stdio: 'inherit' })
+  }
+}
+
 if (!commandName || commandName === COMMAND.BUILD) {
   const configFilePath = path.resolve(__dirname, '..', 'lib', 'rollup.js')
-  shelljs.exec(`rollup --config ${configFilePath}`)
+  run('rollup', ['--config', configFilePath])
   return
 }
 
 if (commandName === COMMAND.LINT) {
-  shelljs.exec(`eslint ${commandParams.join(' ')}`)
+  run('eslint', commandParams)
   return
 }
 
 if (commandName === COMMAND.TEST) {
-  shelljs.exec(`jest ${commandParams.join(' ')}`)
-  return
+  run('jest', commandParams)
 }
